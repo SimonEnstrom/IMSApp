c1a3617d39b4bd4727024dbd1f13312558b0ef88
JoacimLilja <lijo1729@student.ju.se>
2020-04-13 13:07:38 +0200

    added buttons to manual drive screen

--- screens/ManualDrive.js
+++ screens/ManualDrive.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import { Platform, StyleSheet, Text, View, Button, Image } from "react-native";
 class ManualDriveActivity extends React.Component {
     static navigationOptions = {
     title: "ManualDrive",
@@ -9,12 +9,16 @@ class ManualDriveActivity extends React.Component {
 };
 render() {
     return (
-    <View style={styles.container}>
-    <Text style={styles.headerText}> Go Back </Text>
-    <Button
-        title="Go Back"
-        onPress={() => this.props.navigation.goBack()}
-    />
+    <View style={styles.buttonContainer}>
+        <Text style={styles.headerText}>Manual Drive Buttons k</Text>
+        <Image source={require('../img/ManButton.png')} 
+        style={styles.manUp}/>
+        <Image source={require('../img/ManButton.png')} 
+        style={styles.manLeft}/>
+        <Image source={require('../img/ManButton.png')} 
+        style={styles.manRight}/>
+        <Image source={require('../img/ManButton.png')} 
+        style={styles.manDown}/>
     </View>
     );}
 }
@@ -23,13 +27,47 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#F5FCFF"
+        backgroundColor: "rgba(52, 52, 52, 0.0)"
+    },
+    buttonContainer: {
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+        width: '100%'
     },
     headerText: {
         fontSize: 20,
         textAlign: "center",
         margin: 10,
         fontWeight: "bold"
+    },
+    manUp: {
+        width: '20%',
+        height: '20%',
+        margin: 0,
+        padding: 0,
+    },
+    manDown: {
+        width: '20%',
+        height: '20%',
+        transform: [{ rotate: '180 deg'}],
+        margin: 0,
+        padding: 0
+    },
+    manLeft: {
+        width: '20%',
+        height: '20%',
+        transform: [{ rotate: '270 deg'}],
+        margin: 0,
+        padding: 0,
+    },
+    manRight: {
+        width: '20%',
+        height: '20%',
+        transform: [{ rotate: '90 deg'}],
+        margin: 0,
+        padding: 0
     }
 });
 export default ManualDriveActivity;
